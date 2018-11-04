dbef26d156bc653f8f1f719e20e80cfc8da63424
Scarlet <info_ng@me.com>
2018-11-04 19:06:01 +0800

    Initial Commit

---
+++ WKWhatsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WKWhatsApp
+//
+//  Created by Scarlet on 4/11/2018.
+//  Copyright © 2018 Scarlet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
